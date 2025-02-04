022fc2549ad160187a08d30bb03ab01307462fc0
ccerv1 <carl.cervone@gmail.com>
2025-02-04 11:44:29 -0500

    add: simple version of project-to-project dependency graph

--- eval-algos/scripts/config.py
+++ eval-algos/scripts/config.py
@@ -1,12 +1,13 @@
 GCP_CREDENTIALS = 'credentials.json'         # UPDATE THIS WITH YOUR CREDENTIALS
 GCP_PROJECT_ID = 'opensource-observer'       # UPDATE THIS WITH YOUR PROJECT ID
 MODELS = [
-    # 'projects_v1',
+    'projects_v1',
     'int_superchain_s7_onchain_metrics_by_project',
     'int_superchain_s7_devtooling_metrics_by_project', 
     'int_superchain_s7_onchain_builder_eligibility',
     'int_superchain_s7_trusted_developers',
     'int_superchain_s7_project_to_developer_graph',
+    'int_superchain_s7_project_to_dependency_graph_simple',
     
     ## these are big ones !
     # 'int_superchain_s7_devtooling_repo_eligibility',
